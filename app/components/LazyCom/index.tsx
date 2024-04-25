c90fbaa1a2cd46560c1530f56cac695586f5a6eb
王鹏 <75709526+wp0403@users.noreply.github.com>
2024-04-25 17:36:39 +0800

    Update index.tsx

--- app/components/LazyCom/index.tsx
+++ app/components/LazyCom/index.tsx
@@ -58,6 +58,8 @@ const LazyCom = (props: Props) => {
           {isLoad && (
             <AntImage
               className={`${className} ${style.photography_image_antd}`}
+              width={width}
+              height={width}
               onLoad={() => {
                 setIsAntdLoad(true);
               }}
